d9b4f8f0f3034d143d4528c78b264cb757df9b74
luischernandez <luishernandez01@live.com>
2017-12-03 22:06:08 -0700

    Flag every 5th comment as spam

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
       @posts = Post.all
+      @posts.each_with_index {|post, index|
+          if index % 5 == 0
+              post.title = "** THIS IS POST HAS BEEN FLAGGED AS SPAM BY OUR SYSTEM **"
+          end
+      }
   end
 
   def show
